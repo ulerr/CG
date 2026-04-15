90e84916fed38b7e4ada366a298a05c7085917ff
Rafael Moreira <rafamuller05@gmail.com>
2026-04-15 15:44:43 -0300

    Add target, falta acertar rotação

--- TP1/main.js
+++ TP1/main.js
@@ -4,6 +4,7 @@ import GUI from '../libs/util/dat.gui.module.js'
 import Stats from '../build/jsm/libs/stats.module.js';
 import { createTree1, createTree2 } from './arvore.js';
 import { createAirplane } from './aviao.js';
+import { createTarget } from './target.js';
 import { FlyControls } from '../build/jsm/controls/FlyControls.js';
 import KeyboardState from '../libs/util/KeyboardState.js';
 import {
@@ -16,14 +17,14 @@ import {
 } from "../libs/util/util.js";
 
 let clock = new THREE.Clock();
-let flyOn = true;
+let freeFlight = true;
 let baseColor = "rgb(175, 200, 220)"; // It's important the fog color is the same as the background
 let keyboard = new KeyboardState();
 let scene = new THREE.Scene();    // Create main scene
     scene.fog = new THREE.Fog(baseColor, 1, 100); // ADD FOG TO THE SCENE
 let renderer = initRenderer();    // View function in util/utils
    renderer.setClearColor(baseColor); // Set background to match fog color
-let camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(10.0, 15.0, 0.0);
 camera.up.set(0, 1, 0);
 initDefaultBasicLight(scene, true); // Use default light
@@ -86,6 +87,10 @@ let aviao = createAirplane();
 aviao.position.set(0, 5, -40);
 scene.add(aviao);
 
+let target = createTarget();
+target.position.set(0, 5, -50);
+scene.add(target);
+
 function buildInterface() {
    var controls = new function () {
       this.color = baseColor;
@@ -104,8 +109,8 @@ function keyboardUpdate() {
    keyboard.update();
    if (keyboard.down("enter"))
    {
-       flyOn = !flyOn;
-       flyOn ? loadingMessage.changeMessage("Fly On") : loadingMessage.changeMessage("Fly Off");
+       freeFlight = !freeFlight;
+       freeFlight ? loadingMessage.changeMessage("Fly On") : loadingMessage.changeMessage("Fly Off");
    }
 }
 
@@ -113,7 +118,7 @@ function render() {
    const delta = clock.getDelta();
    stats.update();
    keyboardUpdate();
-   if (flyOn) flyCamera.update(delta);
+   if (freeFlight) flyCamera.update(delta);
    requestAnimationFrame(render);
    renderer.render(scene, camera)
 }
