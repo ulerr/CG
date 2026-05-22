bdfd31358a8d2dce4ee718037d4e8412ee740699
Samuel Montessi <samontessi09@gmail.com>
2026-05-22 15:08:43 -0300

    remove logs

--- TP2/inimigo.js
+++ TP2/inimigo.js
@@ -4,22 +4,14 @@ import {GLTFLoader} from '../build/jsm/loaders/GLTFLoader.js';
 export function createEnemy(scene, position) {
   const loader = new GLTFLoader();
 
-  console.log("createEnemy chamado, posição:", position); // <-- log 1
-
   loader.load(
     './assets/duck.glb',
     function (gltf) {
-      console.log("GLB carregado com sucesso!"); // <-- log 2
       let obj = gltf.scene;
-      obj.scale.set(2, 2, 2); // duck.glb costuma ser ENORME, tente 0.005
+      obj.scale.set(2, 2, 2); 
       obj.position.copy(position);
       obj.userData.isEnemy = true;
       scene.add(obj);
-      console.log("Inimigo adicionado à cena em:", obj.position); // <-- log 3
-    },
-    undefined,
-    function (error) {
-      console.error("ERRO ao carregar GLB:", error); // <-- log 4
     }
   );
 }
